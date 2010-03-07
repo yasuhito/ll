02143f0abbdea96ac6689250e1176f9b6c3f1604
Yasuhito Takamiya <yasuhito@gmail.com>
2010-03-07 16:14:43 +0900

    refactoring

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -2,19 +2,31 @@ require File.join( File.dirname( __FILE__ ), "spec_helper" )
 
 
 describe View do
+  before :each do
+    @lock_yasuhito = Lock.new( Chronic.parse( "1979-05-27 05:00" ),
+                               Chronic.parse( "1979-05-27 06:00" ), "yasuhito" )
+    @lock_yutaro = Lock.new( Chronic.parse( "1979-01-31 23:00" ),
+                             Chronic.parse( "1979-02-1 01:00" ), "yutaro" )
+    @messenger = StringIO.new
+  end
+
+
+  it "should show locks" do
+    View.new( @messenger ).show "tick001", [ @lock_yasuhito, @lock_yutaro ]
+    @messenger.string.should == <<-EOF
+tick001:
+  [yasuhito] 1979/05/27 (Sun) 05:00 - 06:00
+  [yutaro] 1979/01/31 (Wed) 23:00 - 02/01 (Thu) 01:00
+EOF
+  end
+
+
   it "should show locks with index" do
-    from0 = Chronic.parse( "1979-05-27 05:00" )
-    lock0 = Lock.new( from0, from0 + 3600, "yasuhito" )
-
-    from1 = Chronic.parse( "1979-01-31 23:00" )
-    lock1 = Lock.new( from1, from1 + 7200, "yasuhito" )
-
-    messenger = StringIO.new
-    View.new( messenger ).show_with_index "tick001", [ lock0, lock1 ]
-    messenger.string.should == <<-EOF
+    View.new( @messenger ).show_with_index "tick001", [ @lock_yasuhito, @lock_yutaro ]
+    @messenger.string.should == <<-EOF
 tick001:
   0) [yasuhito] 1979/05/27 (Sun) 05:00 - 06:00
-  1) [yasuhito] 1979/01/31 (Wed) 23:00 - 02/01 (Thu) 01:00
+  1) [yutaro] 1979/01/31 (Wed) 23:00 - 02/01 (Thu) 01:00
 EOF
   end
 end
