c83d132160ab79ac831b35f001ca08f126006f3e
Yasuhito TAKAMIYA <yasuhito@takamiya.(none)>
2010-03-24 15:01:20 +0900

    refactoring specs for Lock class

--- spec/lock_spec.rb
+++ spec/lock_spec.rb
@@ -15,8 +15,54 @@ Spec::Matchers.define :be_shortened_into do | string |
 end
 
 
+Spec::Matchers.define :be_older_than do | other_lock |
+  match do | lock |
+    ( lock <=> other_lock ) == -1
+  end
+end
+
+
+Spec::Matchers.define :be_newer_than do | other_lock |
+  match do | lock |
+    ( lock <=> other_lock ) == 1
+  end
+end
+
+
+describe Lock, "1979-05-27 04:00 - 1979-05-27 05:00" do
+  subject { new_lock( :from => "1979-05-27 04:00", :to => "1979-05-27 05:00" ) }
+
+
+  context "when compared with an another lock" do
+    it { should be_older_than( new_lock( :from => "1979-05-27 05:00", :to => "1979-05-27 06:00" ) ) }
+    it { should == new_lock( :from => "1979-05-27 04:00", :to => "1979-05-27 05:00" ) }
+    it { should_not == new_lock( :from => "1980-05-27 04:00", :to => "1980-05-27 05:00" ) }
+    it { should be_newer_than( new_lock( :from => "1979-05-27 03:00", :to => "1979-05-27 04:00" ) ) }
+  end
+
+
+  context "when detecting overwrap" do
+    it { should overwrap_with( :from => "1979-05-27 03:00", :to => "1979-05-27 06:00" ) }
+    it { should overwrap_with( :from => "1979-05-27 03:00", :to => "1979-05-27 04:30" ) }
+    it { should overwrap_with( :from => "1979-05-27 04:30", :to => "1979-05-27 06:00" ) }
+    it { should overwrap_with( :from => "1979-05-27 04:15", :to => "1979-05-27 04:45" ) }
+    it { should_not overwrap_with( :from => "1979-05-27 06:00", :to => "1979-05-27 07:00" ) }
+  end
+
+
+  context "when calculating its duration" do
+    its( :duration ) { should == ChronicDuration.parse( "1 hour" ) }
+  end
+
+
+  context "when testing its lifetime" do
+    it { should be_obsolete }
+  end
+end
+
+
 describe Lock, "with 1 hour of duration from now" do
-  context "when testing its liveness" do
+  context "when testing its lifetime" do
     now = Time.now
     subject { Lock.new( now, now + ChronicDuration.parse( "1 hour" ), "yutaro" ) }
     it { should_not be_obsolete }
@@ -24,77 +70,27 @@ describe Lock, "with 1 hour of duration from now" do
 end
 
 
-describe Lock, "1979-05-27 (Sun) 04:00 - 06:00" do
-  before :each do
-    @lock = new_lock( :from => "1979-05-27 04:00", :to => "1979-05-27 06:00" )
-  end
-
-
-  context "when testing its liveness" do
-    subject { @lock }
-    it { should be_obsolete }
-  end
-
-
-  context "when detecting overwrap" do
-    subject { @lock }
-    it { should overwrap_with( :from => "1979-05-27 05:00", :to => "1979-05-27 07:00" ) }
-    it { should overwrap_with( :from => "1979-05-27 03:00", :to => "1979-05-27 05:00" ) }
-    it { should overwrap_with( :from => "1979-05-27 03:00", :to => "1979-05-27 07:00" ) }
-    it { should overwrap_with( :from => "1979-05-27 05:00", :to => "1979-05-27 05:30" ) }
-    it { should_not overwrap_with( :from => "1979-05-27 07:00", :to => "1979-05-27 08:00" ) }
-  end
-
-
-  context "when calculating its duration" do
-    subject { @lock }
-    its( :duration ) { should == ChronicDuration.parse( "2 hours" ) }
-  end
-
-
-  context "when compared with an another lock" do
-    subject { @lock }
-    it { should == new_lock( :from => "1979-05-27 04:00", :to => "1979-05-27 06:00" ) }
-    it { should_not == new_lock( :from => "1979-05-27 05:00", :to => "1979-05-27 06:00" ) }
-  end
+describe Lock, "when its duration falls within a day" do
+  subject { new_lock( :from => "1979-05-27 05:00", :to => "1979-05-27 06:00", :user => "yutaro" ) }
+  it { should be_shortened_into( "[yutaro] 1979/05/27 (Sun) 05:00 - 06:00" ) }
 end
 
 
-describe Lock, "when sorted" do
-  it "should be compared by <=>" do
-    lock_old = new_lock( :from => "1979-05-27 04:00", :to => "1979-05-27 05:00" )
-    lock_new = new_lock( :from => "1979-05-27 05:00", :to => "1979-05-27 06:00" )
-    ( lock_old <=> lock_new ).should == -1
-    ( lock_old <=> lock_old ).should == 0
-    ( lock_new <=> lock_new ).should == 0
-    ( lock_new <=> lock_old ).should == 1
-  end
+describe Lock, "when its duration crosses 00:00" do
+  subject { new_lock( :from => "1979-05-27 23:00", :to => "1979-05-28 01:00", :user => "yutaro" ) }
+  it { should be_shortened_into( "[yutaro] 1979/05/27 (Sun) 23:00 - 05/28 (Mon) 01:00" ) }
 end
 
 
-describe Lock, "when represented as a string" do
-  context "when its duration falls within a day" do
-    subject { new_lock( :from => "1979-05-27 05:00", :to => "1979-05-27 06:00", :user => "yutaro" ) }
-    it { should be_shortened_into( "[yutaro] 1979/05/27 (Sun) 05:00 - 06:00" ) }
-  end
+describe Lock, "when its duration crosses the last day of a month" do
+  subject { new_lock( :from => "1979-01-31 23:00", :to => "1979-02-1 01:00", :user => "yutaro" ) }
+  it { should be_shortened_into( "[yutaro] 1979/01/31 (Wed) 23:00 - 02/01 (Thu) 01:00" ) }
+end
 
 
-  context "when its duration crosses 00:00" do
-    subject { new_lock( :from => "1979-05-27 23:00", :to => "1979-05-28 01:00", :user => "yutaro" ) }
-    it { should be_shortened_into( "[yutaro] 1979/05/27 (Sun) 23:00 - 05/28 (Mon) 01:00" ) }
-  end
-
-
-  context "when its duration crosses the last day of a month" do
-    subject { new_lock( :from => "1979-01-31 23:00", :to => "1979-02-1 01:00", :user => "yutaro" ) }
-    it { should be_shortened_into( "[yutaro] 1979/01/31 (Wed) 23:00 - 02/01 (Thu) 01:00" ) }
-  end
-
-
-  context "when its duration crosses a new year's day" do
-    subject { new_lock( :from => "1978-12-31 23:00", :to => "1979-01-01 01:00", :user => "yutaro" ) }
-    it { should be_shortened_into( "[yutaro] 1978/12/31 (Sun) 23:00 - 1979/01/01 (Mon) 01:00" ) }
-  end
+describe Lock, "when its duration crosses a new year's day" do
+  subject { new_lock( :from => "1978-12-31 23:00", :to => "1979-01-01 01:00", :user => "yutaro" ) }
+  it { should be_shortened_into( "[yutaro] 1978/12/31 (Sun) 23:00 - 1979/01/01 (Mon) 01:00" ) }
 end
 
 
